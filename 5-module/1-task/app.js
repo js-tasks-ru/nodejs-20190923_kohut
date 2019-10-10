042ae5d9514646cf1a72bf78e9641f33e4d4abcb
Denys Kohut <Denys_Kohut@epam.com>
2019-10-10 11:42:01 +0300

    [5-module][1-task] created chat on Koa.js

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,16 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const Chat = require('./chat');
+
 app.use(require('koa-static')('public'));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const chat = new Chat();
 
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await chat.subscribe(ctx);
 });
 
 router.post('/publish', async (ctx, next) => {
+  chat.publish(ctx);
+  ctx.body = '';
 });
 
 app.use(router.routes());
