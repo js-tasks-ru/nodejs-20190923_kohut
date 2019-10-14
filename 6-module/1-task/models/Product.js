11a4cb1bb1fb21636a57cf15ccab7b958656a9a9
Denys Kohut <Denys_Kohut@epam.com>
2019-10-14 19:05:30 +0300

    [6-module][1-task] added Category and Product schemas

--- 6-module/1-task/models/Product.js
+++ 6-module/1-task/models/Product.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    validate: [{
+      validator: (price) => {
+        return price > 0;
+      },
+      message: 'price should be more then 0',
+    }],
+  },
+  category: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  images: [
+    String,
+  ],
 });
 
 module.exports = connection.model('Product', productSchema);
