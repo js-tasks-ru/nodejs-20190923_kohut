0b0f4dbf66bb2cbd285ab4e815ea314a0b451b43
Denys Kohut <Denys_Kohut@epam.com>
2019-10-19 17:20:45 +0300

    [7-module][3-task] added sesion document to db, check token and resctric unauthorized access

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -43,7 +43,12 @@ const router = new Router({prefix: '/api'});
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
-
+  const token = header.split(' ')[1];
+  if (!token) return next();
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) ctx.throw(401, 'Неверный аутентификационный токен');
+  await session.update({lastVisit: new Date});
+  ctx.user = session.user;
   return next();
 });
 
@@ -56,7 +61,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
--- 7-module/3-task/controllers/login.js
+++ 7-module/3-task/controllers/login.js
@@ -1,4 +1,5 @@
 const passport = require('../libs/passport');
+const Session = require('../models/Session');
 
 module.exports.login = async function login(ctx, next) {
   await passport.authenticate('local', async (err, user, info) => {
@@ -11,6 +12,15 @@ module.exports.login = async function login(ctx, next) {
     }
 
     const token = await ctx.login(user);
+    try {
+      await Session.create({
+        token,
+        user: user.id,
+        lastVisit: new Date(),
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
     ctx.body = {token};
   })(ctx, next);
