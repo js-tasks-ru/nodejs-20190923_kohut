7bd9dc32da89d230d85b09b13e12dfc9171f9b07
Denys Kohut <Denys_Kohut@epam.com>
2019-10-29 10:04:03 +0200

    [9-module][1-task] created chat with WebSockets

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,23 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const query = socket.handshake.query;
+    if (!query.token) return next(new Error('anonymous sessions are not allowed'));
+    const session = await Session.findOne({token: query.token}).populate('user');
+    socket.user = session.user;
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const {displayName, id} = socket.user;
+      await Message.create({
+        user: displayName,
+        chat: id,
+        text: msg,
+        date: new Date(),
+      });
+    });
   });
 
   return io;
